b4730988cc6d86a8e9b83d1f94d65375d60ee4cc
mukaihiroto <mukaihiroto@teawm-lab.com>
2018-03-13 03:18:49 +0900

    restAPIの基本機能
    不必要コード削除

--- src/main/java/teamlab/rest/controller/api/ProductApiController.java
+++ src/main/java/teamlab/rest/controller/api/ProductApiController.java
@@ -1,15 +1,9 @@
 package teamlab.rest.controller.api;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.List;
 import java.util.Optional;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,7 +19,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
