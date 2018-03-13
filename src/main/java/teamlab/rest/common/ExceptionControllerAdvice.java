ec75a5edbc39238a3bdcd82c296c8e71969ddce1
mukaihiroto <mukaihiroto@teawm-lab.com>
2018-03-13 15:46:48 +0900

    restAPIの基本機能
    エラーハンドリング

---
+++ src/main/java/teamlab/rest/common/ExceptionControllerAdvice.java
@@ -0,0 +1,26 @@
+package teamlab.rest.common;
+
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class ExceptionControllerAdvice {
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		throw new RuntimeException("controller advice: init binder");
+	}
+	
+	@ExceptionHandler(Exception.class)
+	public String exception(Exception e) {
+		throw new RuntimeException(e.getMessage(),e);
+	}
+	
+	@ModelAttribute
+	public void modelAttribute(){
+		throw new RuntimeException("controller advice:model Attribute");
+	}
+}
